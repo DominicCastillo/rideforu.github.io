36add065186ed0bf0fcb4063aa44d18d160ca383
NATT-CODE <natalyrodriguez.raices2024@gmail.com>
2024-05-09 20:55:51 -0600

    Avance Nataly 1.2

--- ajustes.php
+++ ajustes.php
@@ -4,8 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajustes</title>
+    <link rel="stylesheet" href="./css/ajustes.css">
 </head>
 <body>
-    
+    <div class="main">
+        <a href="#">
+            <img class="back" src="./assets/img/left-arrow.png" alt="">
+        </a>
+        <h2>Ajustes</h2>
+        <center>
+            <img class="control" src="./assets/img/control.png" alt="">
+        </center>
+
+        <div class="btn">
+            <a href="" class="button-link">
+                <img class="item" src="./assets/img/pencil.png" alt="">
+                <span>Editar perfil</span>
+                <img class="arrow" src="./assets/img/right-arrow.png" alt="">
+            </a>
+        </div>
+        <br>
+        <div class="btn">
+            <a href="" class="button-link">
+                <img class="item" src="./assets/img/log-out.png" alt="">
+                <span>Cerrar sesión</span>
+                <img class="arrow" src="./assets/img/right-arrow.png" alt="">
+            </a>
+        </div>
+        
+    </div>
 </body>
 </html>
