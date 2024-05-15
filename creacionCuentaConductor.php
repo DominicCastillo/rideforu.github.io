a9f7f33815ec6406d5eb1a903705436c7f6b9dd5
DominicCastillo <dominic.castillo.pms@gmail.com>
2024-05-15 10:40:04 -0600

    Parte de Formulario Conductor

---
+++ creacionCuentaConductor.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="css/creacionCuentaConductor.css">
+    <title>Inicio Sesión</title>
+</head>
+<body>
+    
+    <center class="mainForm">
+
+        <h2>Crea tu cuenta de usuario.</h2><br><br><br>
+
+        <form class="singInForm" action="" method="POST">
+
+            <label class="formLabel">Crea un Usuario</label><br>
+            <input class="formInput" type="text" name="user" id="user"><br><br>
+
+            <label class="formLabel">Crea una contraseña</label><br />
+            <input class="formInput" type="password" name="password" id="password"><br><br>
+
+            <label class="formLabel">Confirma tu contraseña</label><br />
+            <input class="formInput" type="password" name="password" id="password"><br><br>
+
+            <a href="ingresarDatosVehiculo.php"><input type="submit" value="Siguiente"><br><br></a>
+
+        </form>
+    </center>
+
+</body>
+</html>
--- inicioSesionConductor.php
+++ inicioSesionConductor.php
@@ -10,29 +10,30 @@
 </head>
 <body>
     
-    <center class="mainForm">
+<center class="mainForm">
 
-        <h2>Iniciar Sesión</h2><br><br><br>
+<h2>Iniciar Sesión</h2><br><br><br>
 
-        <form class="singInForm" action="" method="POST">
+<form class="singInForm" action="" method="POST">
 
-            <label class="formLabel">Correo electrónico</label><br>
-            <input class="formInput" type="text" name="email" id="email"><br>
+<label class="formLabel">Correo electrónico</label><br>
+<input class="formInput" type="text" name="email" id="email"><br>
 
-            <label class="formLabel">Contraseña</label><br />
-            <input class="formInput" type="password" name="password" id="password"><br>
+<label class="formLabel">Contraseña</label><br />
+<input class="formInput" type="password" name="password" id="password"><br>
 
-            <a class="forgotPassword" href="#">¿Olvidaste tu Contraseña?</a><br><br>
+<a class="forgotPassword" href="#">¿Olvidaste tu Contraseña?</a><br><br>
 
-            <input type="submit" value="Iniciar Sesión"><br><br>
+<input type="submit" value="Iniciar Sesión"><br><br>
 
-            <a class='googleButton' href="#"><button>Google</button></a><br><br>
+<a class='googleButton' href="#"><button>Google</button></a><br><br>
 
-            <a href="#">¿No tienes una cuenta?</a><br>
+<a href="#">¿No tienes una cuenta?</a><br>
 
-            <a href="ingresoDatos.php"><strong>Regístrate</strong></a>
-        </form>
-    </center>
+<a href="ingresoDatos.php"><strong>Regístrate</strong></a>
+</form>
+
+</center>
 
 </body>
 </html>
