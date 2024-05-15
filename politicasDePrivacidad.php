b014c8ea4d22b587e861689fe77457978505af2c
DominicCastillo <dominic.castillo.pms@gmail.com>
2024-05-15 09:59:39 -0600

    Conductor

---
+++ politicasDePrivacidad.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <link rel="stylesheet" href="css/politicasDePrivacidad.css">
+    <title>Politicas de Privacidad - Ride4u</title>
+</head>
+<body>
+    
+    <div class="mainStyle">
+
+    <center>
+    <img src="assets/img/cat.jpg" alt="Logo Ride4u" width="80px" height="80px">
+    
+    <br><br>
+
+    <p class="mainText">¡Bienvenido a nuestra comunidad de Ride 4U para estudiantes universitarios en El Salvador!<br><br>
+        Estamos aquí para ayudarte a compartir viajes en carro de manera conveniente, económica y amigable.<br><br>
+        Únete a nosotros para ahorrar dinero, reducir tu huella de carbono y hacer nuevos amigos en el camino. 
+        ¡Gracias por formar parte de nuestra emocionante aventura de Ride 4U!
+    </p>
+    
+    <br><br>
+
+    <table>
+        <ul>
+            <li class="mainRules">
+            Conveniencia: Ofrecemos una experiencia de viaje excepcional, simplificando tus desplazamientos diarios a través de una plataforma fácil de usar.
+            </li>
+            <br>
+            <li class="mainRules">
+            Economía: Nuestro objetivo es ayudarte a ahorrar dinero proporcionándote una opción asequible y eficiente para tus trayectos universitarios.
+            </li>
+        </ul>
+    </table>
+
+    <br>
+
+    <div>
+    <input type="checkbox" name="" id=""><p class="checkBoxText">Acepto los términos de privacidad</p>     
+    </div>
+       
+    </center>
+    
+
+    </div>
+
+</body>
+</html>
