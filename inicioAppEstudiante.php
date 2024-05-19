f332de6b0d926d675777210590ed2a3cb80740b5
NATT-CODE <natalyrodriguez.raices2024@gmail.com>
2024-05-19 14:32:37 -0600

    Avance Nataly 2.3

--- inicioAppEstudiante.php
+++ inicioAppEstudiante.php
@@ -68,7 +68,7 @@
 
     <div class="navbar">
         <div class="itemNav">
-        <a class="active" href="inicioApp.php"><img src="./assets/img/home.png" alt="">inicio</a> 
+        <a class="active" href="inicioAppEstudiante.php"><img src="./assets/img/home.png" alt="">inicio</a> 
         </div>
         <div class="itemNav">
         <a href="agendar.php" ><img src="./assets/img/chat.png" alt="">agenda</a> 
@@ -77,7 +77,7 @@
         <a href="#" ><img src="./assets/img/calendar.png" alt="">chat</a> 
         </div>
         <div class="itemNav">
-        <a href="#" ><img src="./assets/img/bell-ring.png" alt="">notificaciones</a>
+        <a href="notificacionesEstudiante.php" ><img src="./assets/img/bell-ring.png" alt="">notificaciones</a>
         </div>
         <div class="itemNav">
         <a href="#" ><img src="./assets/img/user.png" alt="">perfil</a>
